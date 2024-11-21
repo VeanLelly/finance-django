b2d0159268ef27316013444672b981200ae3da69
Vanessa D. <vanessalelly2122@gmail.com>
2024-11-21 17:20:11 -0300

    erros

---
+++ finance/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Category, Transaction
+from .serializers import CategorySerializer,TransactionSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objetc.filter(user=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
